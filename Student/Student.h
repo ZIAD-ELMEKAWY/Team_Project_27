fb058e5596029897e49c41bbd17e6c05d53d0fb2
ZIAD_ELMEKAWY <ziadelmekawy7@gmail.com>
2023-05-11 18:31:42 +0300

    modify paramters in (Student_Login_Check)

--- Student/Student.h
+++ Student/Student.h
@@ -3,8 +3,9 @@
 #ifndef _STUDENT_H_
 #define _STUDENT_H_
 
-int  Student_Login_Check   (int Student_ID , char* Student_Passowrd);
+int  Student_Login_Check   (int Student_ID , char Student_Passowrd);
 void Student_View_Record   (int Student_ID);
 char Student_Edit_Name     (char* Student_Edit_Name);
-char Student_Edit_Paasword (char* Student_Edit_Paasword);
+char Student_Edit_Paasword (char Student_Edit_Paasword);
+
 #endif
