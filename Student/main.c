95c12ea60bed8836dd726c8b10eb702cb225b083
ZIAD_ELMEKAWY <ziadelmekawy7@gmail.com>
2023-05-14 19:13:51 +0300

    This file contains the main module --> Source code

--- Student/main.c
+++ Student/main.c
@@ -3,9 +3,18 @@
 #include "Admin.h"
 #include "Student.h"
 
+
+/**
+* @file     main.c
+* @version  20.03
+* @brief    main Module
+* @details  This file contains the main module Source file.
+* @Author   Ziad_Elmekawy
+**/
+
 int main()
 {
-    int modeChoice;
+    int modeChoice;               /**** Mode Choise ****/
 
     printf("Student Record System\n");
     printf("1. Admin Mode\n");
@@ -15,18 +24,83 @@ int main()
     scanf("%d", &modeChoice);
     fflush(stdin);
 
-    switch (modeChoice)
+    if(modeChoice == 1)         /**** Admin Choice ****/
     {
-    case 1:
-        Admin_Mode();
-        break;
-    case 2:
-        Student_Mode();
-        break;
-    default:
-        printf("Invalid mode choice. Exiting...\n");
-        break;
-    }
+        int adminChoice;
 
+        printf("\nAdmin Mode\n");
+        printf("Choose an option (1-6): \n");
+        printf("1. Add student record\n");
+        printf("2. Remove student record\n");
+        printf("3. View student record\n");
+        printf("4. View all records\n");
+        printf("5. Edit admin password\n");
+        printf("6. Edit student grade\n");
+        fflush(stdin);
+        scanf("%d", &adminChoice);
+        fflush(stdin);
+
+        switch (adminChoice)
+        {
+        case 1:
+            addStudentRecord();
+            break;
+        case 2:
+            removeStudentRecord();
+            break;
+        case 3:
+            viewStudentRecord();
+            break;
+        case 4:
+            viewAllRecords();
+            break;
+        case 5:
+            editAdminPassword();
+            break;
+        case 6:
+            editStudentGrade();
+            break;
+        default:
+            printf("Invalid option. Exiting admin mode...\n");
+            break;
+        }
+    }
+    else if(modeChoice == 2)    /*** Student Choice ***/
+    {
+         int StudentChoice;
+        printf("\nStudent Mode\n");
+        printf("Choose an option (1-4): \n");
+        printf("1_ View your record    \n");
+        printf("2_ Edit your Name      \n");
+        printf("3_ Edit your Passowrd  \n");
+        printf("4_ Go back to main     \n");
+        fflush(stdin);
+        scanf("%d", &StudentChoice);
+        fflush(stdin);
+        switch (StudentChoice)
+        {
+        case 1:
+            Student_View_Record();
+            break;
+        case 2:
+            Student_Edit_Name();
+            break;
+        case 3:
+            Student_Edit_Passowrd();
+            break;
+        case 4:
+            Student_Mode();
+            break;
+        default:
+            printf("Invalid option. Exiting student mode...\n");
+            break;
+        }
+    }
+    else                        /*** Invalid Choice ****/
+    {
+        printf("Invalid value, Sorry...\n");
+    }
     return 0;
+
 }
+
