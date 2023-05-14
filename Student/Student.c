b5d115ac5124d15de63f2cc5e559335cc3f126c8
ZIAD_ELMEKAWY <ziadelmekawy7@gmail.com>
2023-05-14 19:12:58 +0300

    This file contains the Student module --> Source code

--- Student/Student.c
+++ Student/Student.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "Student.h"
 #include <windows.h>
-
-
+#include "Student.h"
+#include "List.h"
 /**
 * @file     Student.c
 * @version  20.03
@@ -18,35 +17,7 @@ void Student_Mode()
 {
     if (1== Student_Login_Check(1,1))
     {
-        int StudentChoice;
 
-        printf("\nStudent Mode\n");
-        printf("Choose an option (1-4): \n");
-        printf("1_ View your record    \n");
-        printf("2_ Edit your Name      \n");
-        printf("3_ Edit your Passowrd  \n");
-        printf("4_ Go back to main     \n");
-        fflush(stdin);
-        scanf("%d", &StudentChoice);
-        fflush(stdin);
-        switch (StudentChoice)
-        {
-        case 1:
-            Student_View_Record(1);
-            break;
-        case 2:
-            Student_Edit_Name();
-            break;
-        case 3:
-            Student_Edit_Passowrd();
-            break;
-        case 4:
-            Student_Mode();
-            break;
-        default:
-            printf("Invalid option. Exiting student mode...\n");
-            break;
-        }
     }
     else
     {
@@ -54,10 +25,11 @@ void Student_Mode()
     }
 }
 
-int Student_Login_Check (int Student_ID , char Student_Passowrd)
+int  Student_Login_Check(int Student_ID, char* Student_Passowrd)
 {
     int status =0;
-    if ((Student_ID == 1) && (Student_Passowrd == 1))
+
+    if ((isIDExist(Student_ID) == 1) && (isIDExist(Student_Passowrd) == 1))  //Modification : there is no function for Check Student Password
     {
         status = 1;
     }
@@ -65,44 +37,43 @@ int Student_Login_Check (int Student_ID , char Student_Passowrd)
     {
         status = 0;
     }
+
     return status ;
 }
 
-void Student_View_Record(int Student_ID)  // Student id isn't valid now
-{
-    if (1 == Student_Login_Check)
-    {
-        printf("your record : %d", Student_record);    /** Student record variable **/
-    }
-    else
-    {
 
-    }
+void Student_View_Record (int Student_ID)   // Function of (View_student_record) in [list.h] will print the record after pass id of student
+{
+    View_student_record(Student_ID);        // if the function will print inside it
 }
+
 
 char Student_Edit_Name(char* Student_Edit_Name[])
 {
-    if (1 == Student_Login_Check)
-    {
-        printf("Please Enter your name : ");
-        gets(Student_Edit_Name);
-    }
+    printf("Please Enter your name : ");
+    gets(Student_Edit_Name);
+    // Function To Edit Student Name isn't valid Now
 }
-void Student_Edit_Passowrd(void)
+
+
+char Student_Edit_Paasword(char* Student_Edit_Paasword)
 {
     int pass = 0;
     printf("Enter your New Password : ");
     scanf("%c",pass);
+// Function To Edit Student Name isn't valid Now
     Motion();
-    printf("Your Name is Modified Successfully \n");
+    printf("Your Password is Modified Successfully \n");
 }
+
+
 void Motion (void)
 {
     printf("Please wait ");
-        for(int UserPassIndex = 0; UserPassIndex < 15; UserPassIndex++)
-        {
-            printf(".");
-            Sleep(100);
-        }
-        printf("\n");
+    for(int UserPassIndex = 0; UserPassIndex < 15; UserPassIndex++)
+    {
+        printf(".");
+        Sleep(100);
+    }
+    printf("\n");
 }
