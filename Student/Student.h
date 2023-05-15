5b4e9433b9e3988de6e32c4f4f51d1ce721eb662
ZIAD_ELMEKAWY <ziadelmekawy7@gmail.com>
2023-05-16 02:34:07 +0300

    This file contains the Student module --> Header file

--- Student/Student.h
+++ Student/Student.h
@@ -9,11 +9,19 @@
 
 #ifndef _STUDENT_H_
 #define _STUDENT_H_
-void Student_Mode();             // Include the definition of functions
-int  Student_Login_Check   (int Student_ID , char* Student_Passowrd);  // To check Student Login
+#define MAX_PASSWORD_LENGTH 50
+#define MAX_NAME_LENGTH 60
+
+int StudentId ;
+
+
+
+int  Student_Login_Check   (void);  // To check Student Login
 void Student_View_Record   (int Student_ID);               // To view Student Record
-char Student_Edit_Name     (char* Student_Edit_Name[]);    // Edit student Name
-char Student_Edit_Paasword (char* Student_Edit_Paasword);   // Edit student passowrd
+void Student_Edit_Name     (void) ;    // Edit student Name
+void Student_Edit_Paasword (void);   // Edit student passowrd
 void Motion (void);              // For motion not more ^_^
 
 #endif
+
+
