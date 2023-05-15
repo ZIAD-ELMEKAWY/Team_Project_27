1332cc19ac5b95cadde4db1e4fa03218cf96680f
ZIAD_ELMEKAWY <ziadelmekawy7@gmail.com>
2023-05-16 02:35:00 +0300

    This file contains the main module --> Source code

--- Student/main.c
+++ Student/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "Admin.h"
 #include "Student.h"
-
+#include "List.h"
 
 /**
 * @file     main.c
@@ -12,10 +12,12 @@
 * @Author   Ziad_Elmekawy
 **/
 
+
+
 int main()
 {
-    int modeChoice;               /**** Mode Choise ****/
-
+    int modeChoice;
+    /**** Mode Choice Start ****/
     printf("Student Record System\n");
     printf("1. Admin Mode\n");
     printf("2. Student Mode\n");
@@ -23,80 +25,103 @@ int main()
     fflush(stdin);
     scanf("%d", &modeChoice);
     fflush(stdin);
+    /**** Mode Choice Start ****/
 
-    if(modeChoice == 1)         /**** Admin Choice ****/
+    if(modeChoice == 1)         /**** Admin Choice of Mode Choice****/
     {
+
         int adminChoice;
-
-        printf("\nAdmin Mode\n");
-        printf("Choose an option (1-6): \n");
-        printf("1. Add student record\n");
-        printf("2. Remove student record\n");
-        printf("3. View student record\n");
-        printf("4. View all records\n");
-        printf("5. Edit admin password\n");
-        printf("6. Edit student grade\n");
-        fflush(stdin);
-        scanf("%d", &adminChoice);
-        fflush(stdin);
-
-        switch (adminChoice)
+        int ADStaus = 0 ;     /*** For return of Validate_Admin_Password ***/
+        /**** Admin login check Start***/
+        ADStaus = Validate_Admin_Password();
+        if (1 == ADStaus)
         {
-        case 1:
-            addStudentRecord();
-            break;
-        case 2:
-            removeStudentRecord();
-            break;
-        case 3:
-            viewStudentRecord();
-            break;
-        case 4:
-            viewAllRecords();
-            break;
-        case 5:
-            editAdminPassword();
-            break;
-        case 6:
-            editStudentGrade();
-            break;
-        default:
-            printf("Invalid option. Exiting admin mode...\n");
-            break;
+            printf("\nAdmin Mode\n");
+            printf("Choose an option (1-6): \n");
+            printf("1. Add student record\n");
+            printf("2. Remove student record\n");
+            printf("3. View student record\n");
+            printf("4. View all records\n");
+            printf("5. Edit admin password\n");
+            printf("6. Edit student grade\n");
+            fflush(stdin);
+            scanf("%d", &adminChoice);
+            fflush(stdin);
+
+            switch (adminChoice)
+            {
+            case 1:
+                addStudentRecord();
+                break;
+            case 2:
+                removeStudentRecord();
+                break;
+            case 3:
+                viewStudentRecord();
+                break;
+            case 4:
+                viewAllRecords();
+                break;
+            case 5:
+                editAdminPassword();
+                break;
+            case 6:
+                editStudentGrade();
+                break;
+            default:
+                printf("Invalid option. Exiting admin mode...\n");
+                break;
+            }
         }
+        else
+        {
+            printf("Invalid password. Exiting Admin mode...\n");
+        }
+        /**** Admin login check End***/
     }
-    else if(modeChoice == 2)    /*** Student Choice ***/
+    else if(modeChoice == 2)    /*** Student Choice of Mode Choice***/
     {
-         int StudentChoice;
-        printf("\nStudent Mode\n");
-        printf("Choose an option (1-4): \n");
-        printf("1_ View your record    \n");
-        printf("2_ Edit your Name      \n");
-        printf("3_ Edit your Passowrd  \n");
-        printf("4_ Go back to main     \n");
-        fflush(stdin);
-        scanf("%d", &StudentChoice);
-        fflush(stdin);
-        switch (StudentChoice)
+
+        int StudentChoice;
+
+        /**** Student login check Start***/
+        if ( Student_Login_Check() == 1)
         {
-        case 1:
-            Student_View_Record();
-            break;
-        case 2:
-            Student_Edit_Name();
-            break;
-        case 3:
-            Student_Edit_Passowrd();
-            break;
-        case 4:
-            Student_Mode();
-            break;
-        default:
-            printf("Invalid option. Exiting student mode...\n");
-            break;
+            printf("\nStudent Mode\n");
+            printf("Choose an option (1-4): \n");
+            printf("1_ View your record    \n");
+            printf("2_ Edit your Name      \n");
+            printf("3_ Edit your Passowrd  \n");
+            printf("4_ Go back to main     \n");
+            fflush(stdin);
+            scanf("%d", &StudentChoice);
+            fflush(stdin);
+            switch (StudentChoice)
+            {
+            case 1:
+                Student_View_Record(StudentId);
+                break;
+            case 2:
+                Student_Edit_Name();
+                break;
+            case 3:
+                Student_Edit_Passowrd();
+                break;
+            case 4:
+                main();
+                break;
+            default:
+                printf("Invalid option. Exiting student mode...\n");
+                break;
+            }
         }
+        else
+        {
+            printf("Invalid Information. Exiting student mode...\n");
+        }
+        /**** Student login check End***/
     }
-    else                        /*** Invalid Choice ****/
+    else                        /*** Invalid Choice of Mode Choice****/
     {
         printf("Invalid value, Sorry...\n");
     }
