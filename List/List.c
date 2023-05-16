1c399f7a500965da8e96683df0268019719b33a8
abdelrhman_rabie3 <91215468+abdelrhman20202@users.noreply.github.com>
2023-05-17 00:46:11 +0300

    Add files via upload

--- List/List.c
+++ List/List.c
@@ -3,16 +3,16 @@
 #include"List.h"
 
 
-struct n* List_Add_student_record(struct n* start,char *name,int grade,int ID,int age,char* gender,char *password)
+struct n* List_Add_student_record(struct n* start,char name[],int grade,int ID,int age,char gender[],char password[])
 {
 	struct n* temp = (struct n*)malloc(sizeof(struct n));
 
-    temp->studunt_name=name;
+    strcpy(temp->student_name,name);
     temp->total_grade=grade;
     temp->student_ID=ID;
     temp->student_age=age;
-    temp->gender=gender;
-    temp->student_password=password;
+    strcpy(temp->gender,gender);
+    strcpy(temp->student_password,password);
 
 	temp->link=start;
 	start = temp;
@@ -65,10 +65,12 @@ while(ptr!=NULL){
 
 
 
-		printf("Student name is : %d\n",ptr->studunt_name);
+		printf("Student name is :");
+		puts(ptr->student_name);
 		printf("Student grade is :%d\n",ptr->total_grade);
 		printf("Student age is %d\n",ptr->student_age);
-		printf("Student gender is %d\n",ptr->gender);
+		printf("Student gender is:");
+		puts(ptr->gender);
 
 
 	}
@@ -76,7 +78,7 @@ else
     ++count;
 
 		ptr = ptr->link;}
-		int r=SDB_GetUsedSize(start);
+		int r=List_GetUsedSize(start);
 if(count==r){
     printf("enter correct studentID\n");
 }
@@ -85,7 +87,7 @@ if(count==r){
 		}
 
 /*********************************************************************************************/
-int SDB_GetUsedSize(struct n* start)
+int List_GetUsedSize(struct n* start)
 {
 	struct n* temp = start;
     int count=0;
@@ -98,3 +100,92 @@ int SDB_GetUsedSize(struct n* start)
 	return count;
 }
 /*********************************************************************************************/
+void view_all_records(struct n* start)
+{
+    struct n* ptr =start;
+    printf("list is \n");
+
+    while(ptr!=NULL)
+    {
+    printf("student name is:");
+    puts(ptr->student_name);
+
+    printf("total grade is:%d\n",ptr->total_grade);
+    printf("student id is:%d\n",ptr->student_ID);
+    printf("student age is:%d\n",ptr->student_age);
+    printf("gender is:");
+    puts(ptr->gender);
+    printf("\n");
+    ptr=ptr->link;
+    }
+}
+/*********************************************************************************************/
+struct n* List_isIDExist(struct n* start,int id, char password [])
+{
+
+int count=0;
+struct n* ptr = start;
+while(ptr!=NULL){
+	if(ptr->student_ID==id && strcmp(ptr->student_password,password)==0)
+	{return 1;}
+
+
+else
+    ++count;
+
+		ptr = ptr->link;}
+		int r=List_GetUsedSize(start);
+if(count==r){
+    return 0;
+}
+}
+/*********************************************************************************************/
+
+void List_editStudent_password(struct n* start,char OldPass[],char newPass[])
+{
+    int count=0;
+	struct n* ptr = start;
+
+while(ptr!=NULL){
+	if(strcmp(ptr->student_password,OldPass)==0)
+	{
+strcpy(ptr->student_password,newPass);
+
+	}
+else
+    ++count;
+
+		ptr = ptr->link;}
+		int r=List_GetUsedSize(start);
+if(count==r){
+    printf("enter correct password\n");
+}
+
+
+		}
+/*********************************************************************************************/
+
+void List_editStudent_name(struct n* start,int id,char newName[])
+{
+    int count=0;
+	struct n* ptr = start;
+
+while(ptr!=NULL){
+	if(ptr->student_ID==id)
+	{
+strcpy(ptr->student_name,newName);
+
+	}
+else
+    ++count;
+
+		ptr = ptr->link;}
+		int r=List_GetUsedSize(start);
+if(count==r){
+    printf("enter correct ID\n");
+}
+
+
+		}
+
+
--- List/List.h
+++ List/List.h
@@ -2,20 +2,24 @@
 #define LIST_H_
 
 struct n
-{   char* studunt_name;
+{   char student_name[500];
     int total_grade;
     int student_ID;
     int student_age;
-    char* gender;
-    char* student_password;
+    char gender[50];
+    char student_password[500];
 
 struct n* link;
 
     }* start;
 
 
-struct n* Add_student_record(struct n* start,char *name,int grade,int ID,int age,char gender[50],char *password);
+struct n* List_Add_student_record(struct n* start,char name[],int grade,int ID,int age,char gender[],char password[]);
 struct n* List_DeleteRecord(struct n * start, int ID);
 void List_viewRecord(struct n* start,int ID);
 int List_GetUsedSize(struct n* start);
+void view_all_records(struct n* start);
+struct n* List_isIDExist(struct n* start,int id, char password []);
+void List_editStudent_password(struct n* start,char OldPass[],char newPass[]);
+void List_editStudent_name(struct n* start,int id,char newName[]);
 #endif
