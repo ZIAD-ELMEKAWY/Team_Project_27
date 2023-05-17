f4779267fafcae26b617abba612b8e43be164089
ZIAD_ELMEKAWY <ziadelmekawy7@gmail.com>
2023-05-17 19:37:14 +0300

    This file contains the Admin module --> Header file

--- Admin/admin.h
+++ Admin/admin.h
@@ -2,11 +2,11 @@
 #define ADMIN_H
 
 int Validate_Admin_Password(); // Function to validate admin password
-void Add_Student_Record();     // Function to add a student record
-void Remove_Student_Record();  // Function to remove a student record
-void View_Student_Record();    // Function to view a specific student record
-void View_All_Records();       // Function to view all student records
-void Edit_Admin_Password();    // Function to edit the admin password
-void Edit_Student_Grade();     // Function to edit a student's grade
+struct n* Add_Student_Record(struct n* start);     // Function to add a student record
+void Remove_Student_Record(struct n* start);  // Function to remove a student record
+void View_Student_Record(struct n* start);    // Function to view a specific student record
+void View_All_Records(struct n* start);       // Function to view all student records
+void Edit_Admin_Password(struct n* start);    // Function to edit the admin password
+void Edit_Student_Grade(struct n* start);     // Function to edit a student's grade
 
 #endif
