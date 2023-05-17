98807d876997f2fedb9b79a1acc93de38b04e089
Abdelrhman Mohamed <abdelrahmanellawendy5@gmail.com>
2023-05-17 23:06:32 +0300

    update of admin.h

--- Admin/admin.h
+++ Admin/admin.h
@@ -2,11 +2,31 @@
 #define ADMIN_H
 
 int Validate_Admin_Password(); // Function to validate admin password
-struct n* Add_Student_Record(struct n* start);     // Function to add a student record
-void Remove_Student_Record(struct n* start);  // Function to remove a student record
-void View_Student_Record(struct n* start);    // Function to view a specific student record
-void View_All_Records(struct n* start);       // Function to view all student records
-void Edit_Admin_Password(struct n* start);    // Function to edit the admin password
-void Edit_Student_Grade(struct n* start);     // Function to edit a student's grade
+// Takes nothing.
+// Returns integer.
+
+struct n *Add_Student_Record(struct n *start); // Function to add a student record
+// Takes the start pointer of the linked list.
+// Returns the updated start pointer.
+
+void Remove_Student_Record(struct n *start); // Function to remove a student record
+// Takes the start pointer of the linked list.
+// Returns nothing.
+
+void View_Student_Record(struct n *start); // Function to view a specific student record
+// Takes the start pointer of the linked list.
+// Returns nothing.
+
+void View_All_Records(struct n *start); // Function to view all student records
+// Takes the start pointer of the linked list.
+// Returns nothing.
+
+void Edit_Admin_Password(); // Function to edit the admin password
+// nothing.
+// Returns nothing.
+
+void Edit_Student_Grade(struct n *start); // Function to edit a student's grade
+// Takes the start pointer of the linked list.
+// Returns nothing.
 
 #endif
